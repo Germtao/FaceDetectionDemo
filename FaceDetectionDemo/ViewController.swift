40f68a2f3bc9ecd5600e2d755923dbf9657432db
QDSG <piftao1127@163.com>
2019-11-11 15:30:18 +0800

    Initial Commit

---
+++ FaceDetectionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetectionDemo
+//
+//  Created by QDSG on 2019/11/11.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
